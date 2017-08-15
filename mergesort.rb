dc0b2dad729374c3e1a90ebfee01b0678ee11a01
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-14 22:13:53 -0700

    merge_sort complete

--- mergesort.rb
+++ mergesort.rb
@@ -1,10 +1,31 @@
-#initialize file
+def merge(l, r)
+  arr = []
 
-def merge_sort(n)
-  if n < 2
-    return
+  while l.size > 0 && r.size > 0
+    if l[0] < r[0]
+	  arr << l.shift
+	else
+	  arr << r.shift
+	end
+  end
+
+  if l.empty?
+    arr += r
   else
-    sort left half of elements
-    sort right half of elements
-    merge sorted halves 
+    arr += l
+  end
+
+  arr
 end
+
+def merge_sort(arr)
+  return arr if arr.size <= 1
+
+  l = merge_sort(arr[0...(arr.length / 2)])
+  r = merge_sort(arr[(arr.length / 2)..-1])
+  merge(l, r)
+end
+
+input_array = [2,8,4,3,9,1,5,7,6,0]
+puts "\nOriginal array: #{input_array}"
+puts "Sorted array: #{merge_sort(input_array)}"
